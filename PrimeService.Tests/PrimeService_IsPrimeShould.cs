37c601874e13aede5842f17a199abf8e8eb5a01c
Eric Roy <ericr@xello.world>
2021-11-23 14:28:46 -0500

    3rd failing test

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -15,9 +15,16 @@ namespace PrimeService.UnitTests.Services
         [DataRow(-1)]
         [DataRow(0)]
         [DataRow(1)]
-        public void IsPrime_InputIs1_ReturnFalse(int value)
+        public void IsPrime_ValueLessThan2_ReturnFalse(int value)
         {
             bool result = _primeService.IsPrime(value);
+            Assert.IsFalse(result, "{value} should not be a prime");
+        }
+
+        [TestMethod]
+        public void IsPrime_ValueDivisibleBy2_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(4);
             Assert.IsFalse(result, "{value} should not be a prime");
         }
 
